aa98d2a196de29350497250d302ab2f918033afe
Evgeni Leshtanski <evgenileshtanski@gmail.com>
2022-07-11 10:20:36 +0300

    Initial Commit

---
+++ 04. Greater Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+
+            if (num1 >= num2)
+            {
+                Console.WriteLine("Greater number:{0}", num1);
+            }
+            else
+            {
+                Console.WriteLine("Greater number:{0}", num2);
+            }
+        }
+    }
+}
